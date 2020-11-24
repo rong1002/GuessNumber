f6b7d1a50590981c35d22f1a99f6cb4de41d09e6
rong1002 <borong1002@gmail.com>
2020-11-24 17:16:13 +0800

    Initial Commit

---
+++ GuessNumber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessNumber
+//
+//  Created by Lin Bo Rong on 2020/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by Lin Bo Rong on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
